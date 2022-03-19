018f1464d8677d24c2059ef037730541aa7ff617
Carlos Lerma <cjl628@icloud.com>
2022-03-19 17:17:11 -0400

    routes created

--- server/api/notes.js
+++ server/api/notes.js
@@ -1,14 +1,23 @@
 const router = require('express').Router();
+const { Note } = require('../db/Note');
 
 //GET /api/notes
-router.get('/', (req, res, next) => {
-  //get request, list notes in descending order
-  console.log('working');
+router.get('/', async (req, res, next) => {
+  try {
+    const notesList = await Note.findAll({ sort: [updatedAt, descending] });
+    res.send(notesList);
+  } catch (error) {
+    next(error);
+  }
 });
 
 //POST /api/notes
-router.post('/', (req, res, next) => {
-  //post request, create new note
+router.post('/', async (req, res, next) => {
+  try {
+    res.status(201).send(await Note.create(req.body));
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const db = require('./db');
+const db = require('./db/db');
 
 const PORT = 8080;
 
