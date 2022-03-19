bd3675f4a1807119d070e7cea3dd9b7691a61ff9
Carlos Lerma <cjl628@icloud.com>
2022-03-19 16:56:15 -0400

    database created

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const app = require('./app');
+const db = require('./db');
 
 const PORT = 8080;
 
-app.listen(PORT, console.log(`app running on port ${PORT}`));
+const init = async () => {
+  await db.sync();
+  app.listen(PORT, console.log(`app running on port ${PORT}`));
+};
+
+init();
